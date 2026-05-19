11568d07bc5b10f02ebca2ad3ee1bd167a98da61
Quent1 <sworkyx@gmail.com>
2026-05-19 06:22:46 -0700

    Correction sur deck nav et écriture de transfert
    questionnement les tools devraient il avoir accès au dimension des objets.
    Peut être  mieux d'ajouter fonctions
    safe_sweep()
    inside_usable_space() ?

--- src/science_jubilee/tools/unique_tools/Inoculator.py
+++ src/science_jubilee/tools/unique_tools/Inoculator.py
@@ -1,11 +1,15 @@
 import random
-import warnings
-from typing import Tuple
 
-import numpy as np
+import math
+import random
+
+from science_jubilee.labware.Labware import (
+    Location,
+    Point,
+    Well,
+)
 
 from science_jubilee.navigation.deck_navigation import DeckNavigator
-from science_jubilee.labware.Labware import Well, Location
 from science_jubilee.tools.Tool import Tool, requires_active_tool
 
 
@@ -27,69 +31,128 @@ class Inoculator(Tool):
     transfert doit accepter en paramètres des puits ou des slots/labware ?
     Exemple de fonction déveleopper ici: transfert d'un puit a un autre
     la position des puits a supposé comme le centre du puits
+
+    Déplacer toute la logique géométrique dans Well ?
+    Exemple:
+    well.random_position()
+    well.safe_sweep()
+
+    Le Tool ne devrait pas connaître ne doit pas avoir accès a la géométrie
+    mais Deck_navigation doit pouvoir utiliser les fonctions de well pour les données aux tools
+    Utilité sur d'autres outils ?
+    d'un point de vue pratique est ce vraiment problématique
     """
 
     @requires_active_tool
     def transfer(
         self,
-        source:  Well = None,
-        destination: Well = None,
+        source: Well,
+        destination: Well,
+        *,
         speed: int = 2000,
-        sweep_x: float = 5,
-        sweep_y: float = 5,
-        sweep_speed: float = 100,
+        sweep_x: float = 5.0,
+        sweep_y: float = 5.0,
+        sweep_speed: float = 100.0,
         randomize_pickup: bool = False,
-    ):
-        #On créé une Location, pour que la machine se déplace a la surface de l'eau
-        position: Location
-        position.x = source.x
-        position.y = source.y
-        position.z = source.depth
-        #si le randomize_pickup est actif
-        #On créé une Location qui sera une position aléatoire dans le puit
-        if (
-                randomize_pickup
-            ):  # to make sure we don't try to pickup from an empty region
-                r = source.diameter / 3 # par sécurité, on réduit le rayon d'action possible 
-                rx = random.randint(-r, r)
-                ry = random.randint(-r, r)
+    ) -> None:
+        """
+        Transfer an object from source well to destination well.
+        """
+        pickup_x = source.x
+        pickup_y = source.y
+        pickup_z = source.depth
 
-                position.x = source.x + rx
-                position.y = source.y + ry
+        rx = 0.0
+        ry = 0.0
 
-        #se déplace vers la source, 
-        #QUESTION z_from top le haut de la Labware ou la surface du liquide
-        navigation.move_to_well(source, z_from_top=0)
+        if randomize_pickup:
 
-        #Tester la vitesse optimale pour limiter l'éparpillement des lentilles
-        navigation.move_to_well(position, speed = sweep_speed)
-        #move to well a une sécurité move to safe Z, qui la rend inutilisable
-        #necessité d'avoir une fonction du style move to location sans safe z
+            if source.diameter is None:
+                raise ValueError(
+                    "Randomized pickup requires a circular well "
+                    "with a defined diameter."
+                )
 
-        #effectue le balayage
-        #créé une position pour chaque mouvement de balayage
-        #TODO ajouter une sécurité pour que  sweep ne nous emmene pas en dehors du puit
-        #sweep pourrait etre un valeur borné 
-        #exemple if well.diameter / 2 - rx - 5  < sweep => sweep = 2
-        position.x += sweep_x
-        position.y += sweep_y
-        navigation.move_to_well(position, speed = sweep_speed)
+            r = (source.diameter / 2 ) * 0.7
 
-        #vitesse optimale a tester
-        #remonte brusquement pour conserver la lentille dans la boucle
-        navigation.move_to_well(source, z_from_top=0)
-        
-        #doit se déplacer a la surface
-        position.x = destination.x
-        position.y = destination.y
-        position.z = destination.depth
-        navigation.move_to_well(position)
+            angle = random.uniform(0, 2 * math.pi)
+            radius = random.uniform(0, r)
 
-        #effectue le balayage
-        #créé une position pour chaque mouvement de balayage
-        position.x += sweep_x
-        position.y += sweep_y
-        navigation.move_to_well(position, speed = sweep_speed)
+            rx = math.cos(angle) * radius
+            ry = math.sin(angle) * radius
+
+            pickup_x += rx
+            pickup_y += ry
+
+        pickup_position = Location(
+            point=Point(
+                pickup_x,
+                pickup_y,
+                pickup_z,
+            ),
+            resource=source,
+        )
+
+        navigation.move_to_target(source, z_from_top=0)
+        navigation.move_to_target(pickup_position, speed_z=sweep_speed, travel_margin=0,safe_movement= False)
+
+        sweep_target_x = pickup_x + sweep_x
+        sweep_target_y = pickup_y + sweep_y
+
+        if source.diameter is not None:
+
+            max_r = (source.diameter / 2) * 0.85
+
+            dx = sweep_target_x - source.x
+            dy = sweep_target_y - source.y
+
+            distance = math.sqrt(dx**2 + dy**2)
+
+            if distance > max_r:
+
+                scale = max_r/ distance
+
+                dx *= scale
+                dy *= scale
+
+                sweep_target_x = source.x + dx
+                sweep_target_y = source.y + dy
+
+        sweep_position = Location(
+            point=Point(
+                sweep_target_x,
+                sweep_target_y,
+                pickup_z,
+            ),
+            resource=source,
+        )
+
+        navigation.move_to_target(sweep_position, speed_z=sweep_speed, travel_margin=0, safe_movement= False)
+        navigation.move_to_target(source,z_from_top=0)
+
+        destination_position = Location(
+            point=Point(
+                destination.x,
+                destination.y,
+                destination.depth,
+            ),
+            resource=destination,
+        )
+
+        navigation.move_to_target(destination_position,travel_margin= 0, safe_movement= True)
+
+        destination_sweep = Location(
+            point=Point(
+                destination.x + sweep_x,
+                destination.y + sweep_y,
+                destination.depth,
+            ),
+            resource=destination,
+        )
+
+        navigation.move_to_target(destination_sweep,speed_z=sweep_speed,travel_margin=0, safe_movement= False)
+        navigation.move_to_safe_z()
+
 
 """
     def transfert_to_all_well(source: str, destination: str)
