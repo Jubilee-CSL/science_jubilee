81f7522976f3e09fc610fb538e485175d3397b3b
Quent1 <sworkyx@gmail.com>
2026-05-21 05:49:07 -0700

    adaptation de la fonction transfert
    On utilise maintenant les fonctions ajouté dans wells

--- src/science_jubilee/tools/unique_tools/Inoculator.py
+++ src/science_jubilee/tools/unique_tools/Inoculator.py
@@ -23,113 +23,53 @@ class Inoculator(Tool):
         """Constructor method"""
         super().__init__(index, name)
 
-    """
-    transfert doit accepter en paramètres des puits ou des slots/labware ?
-    Exemple de fonction déveleopper ici: transfert d'un puit a un autre
-    la position des puits a supposé comme le centre du puits
-
-    Déplacer toute la logique géométrique dans Well ?
-    Exemple:
-    well.random_position()
-    well.safe_sweep()
-
-    Le Tool ne devrait pas connaître ne doit pas avoir accès a la géométrie
-    mais Deck_navigation doit pouvoir utiliser les fonctions de well pour les données aux tools
-    Utilité sur d'autres outils ?
-    d'un point de vue pratique est ce vraiment problématique
-    """
-
     @requires_active_tool
-    def transfer(
-        self,
-        source: Well,
-        destination: Well,
+    def transfer(self, source: Well, destination: Well,
         *,
         speed: int = 2000,
-        sweep_x: float = 5.0,
-        sweep_y: float = 5.0,
-        sweep_speed: float = 100.0,
+        sweep_x: float = 5.0, sweep_y: float = 5.0, sweep_speed: float = 100.0,
         randomize_pickup: bool = False,
     ) -> None:
         """
         Transfer an object from source well to destination well.
         """
-        pickup_x = source.x
-        pickup_y = source.y
-        pickup_z = source.depth
 
-        rx = 0.0
-        ry = 0.0
-
-        if randomize_pickup:
-
-            if source.diameter is None:
-                raise ValueError(
-                    "Randomized pickup requires a circular well "
-                    "with a defined diameter."
-                )
-
-            r = (source.diameter / 2 ) * 0.7
-
-            angle = random.uniform(0, 2 * math.pi)
-            radius = random.uniform(0, r)
-
-            rx = math.cos(angle) * radius
-            ry = math.sin(angle) * radius
-
-            pickup_x += rx
-            pickup_y += ry
-
+        #Définition de la position de départ, 
+        #on cherche a atteindre la surface de l'eau dans le puit
+        #possiblement ajouter un attribut surface au puits ou
+        #définir une quantité d'eau que l'utilisateur doit respecter ???
         pickup_position = Location(
             point=Point(
-                pickup_x,
-                pickup_y,
-                pickup_z,
+                source.x,
+                source.y,
+                source.depth,
             ),
             resource=source,
         )
+
+        if randomize_pickup:
+            pickup_position = source.random_point(source.depth, safety_margin= 0.8)
+
+        #Déplacement vers la position définit comme position de départ
         #tester la vitesse de optimal pour ne pas pousser les lentilles
-        navigation.move_to_target(source, z_from_top=0)
-        navigation.move_to_target(pickup_position, speed_z=sweep_speed, travel_margin=0,safe_movement= False)
+        navigation.move_to_target(pickup_position, 
+                                  speed_z=sweep_speed, 
+                                  travel_margin=4,safe_movement= True)
 
-        sweep_target_x = pickup_x + sweep_x
-        sweep_target_y = pickup_y + sweep_y
+        #Test de validité du mouvement et 
+        #Retour de la position finale du balayage
+        #possibilité d'améliorer la fonction pour obtenir une correction automatique
+        sweep_position = source.safe_sweep(start_point= pickup_position, 
+                sweep_x=sweep_x, sweep_y=sweep_y)
 
-        if source.diameter is not None:
-
-            max_r = (source.diameter / 2) * 0.85
-
-            dx = sweep_target_x - source.x
-            dy = sweep_target_y - source.y
-
-            distance = math.sqrt(dx**2 + dy**2)
-
-            #autre possibilité que la correction automatique
-            #demander à l'utilisateur de donnée de meilleur valeur
-            #ou alors ne plus proposer a l'utilisateur et définir ces valeurs par le calcul
-            if distance > max_r:
-
-                scale = max_r/ distance
-
-                dx *= scale
-                dy *= scale
-
-                sweep_target_x = source.x + dx
-                sweep_target_y = source.y + dy
-
-        sweep_position = Location(
-            point=Point(
-                sweep_target_x,
-                sweep_target_y,
-                pickup_z,
-            ),
-            resource=source,
-        )
-
+        #Mouvement de balayge
         #tester la vitesse de optimal pour attraper la lentille
-        navigation.move_to_target(sweep_position, speed_z=sweep_speed, travel_margin=0, safe_movement= False)
-        navigation.move_to_target(source,z_from_top=0)
+        navigation.move_to_target(sweep_position,
+            speed_z=sweep_speed, 
+            travel_margin=0, safe_movement= False)
 
+        #Définition de la position de départ, 
+        #on cherche a atteindre la surface de l'eau dans le puit
         destination_position = Location(
             point=Point(
                 destination.x,
@@ -139,19 +79,16 @@ class Inoculator(Tool):
             resource=destination,
         )
 
-        navigation.move_to_target(destination_position,travel_margin= 0, safe_movement= True)
+        #Déplacement vers la source
+        navigation.move_to_target(destination_position,
+            travel_margin= 0, safe_movement= True)
 
-        destination_sweep = Location(
-            point=Point(
-                destination.x + sweep_x,
-                destination.y + sweep_y,
-                destination.depth,
-            ),
-            resource=destination,
-        )
+        sweep_position = destination.safe_sweep(
+            start_point= destination_position, 
+            sweep_x=sweep_x, sweep_y=sweep_y)
 
         #tester la vitesse de optimal pour retirer la lentille
-        navigation.move_to_target(destination_sweep,speed_z=sweep_speed,travel_margin=0, safe_movement= False)
+        navigation.move_to_target(sweep_position,speed_z=sweep_speed,travel_margin=0, safe_movement= False)
         navigation.move_to_safe_z()
 
 
