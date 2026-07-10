e124388b4dcacd36fee640a24712ca93fdbfdf30
bouizegareneines-sys <bouizegarene.ines@gmail.com>
2026-07-10 13:42:13 +0200

    Fix formatting

--- tests/test_tools_blender_connection.py
+++ tests/test_tools_blender_connection.py
@@ -3,12 +3,13 @@ from pathlib import Path
 
 
 @pytest.mark.primary
-
 def test_blender(transport):
     script_name = "run_latest_gcode_animation.bat"
-    search_dir = Path(__file__).resolve().parent.parent.parent / "jubilee-blender-twin" / "from_gcode"
-    
-    return_code = transport._inner.launch_twin(script_name,search_dir)
+    search_dir = (
+        Path(__file__).resolve().parent.parent.parent
+        / "jubilee-blender-twin"
+        / "from_gcode"
+    )
+
+    return_code = transport._inner.launch_twin(script_name, search_dir)
     return return_code == 0, f"Blender script failed with return code {return_code}"
-
-
