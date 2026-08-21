a7321f7f25dc1e7b281b0a332ed7c1fdacb960bb
justinveras2546 <justinveras20@gmail.com>
2026-08-21 13:40:45 +0200

    ingredients for sacred implementation

---
+++ src/science_jubilee/Vision/3D_Reconstruction/ingredients/__init__.py
@@ -0,0 +1,11 @@
+"""3D Reconstruction ingredients package."""
+
+__all__ = [
+    "pipeline",
+    "colmap",
+    "reconstruction",
+    "filter_scene",
+    "filter_plants",
+    "scale_by_cameras",
+    "meshing",
+]
