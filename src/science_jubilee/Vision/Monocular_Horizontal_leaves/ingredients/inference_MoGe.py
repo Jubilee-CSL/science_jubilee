39efdfbe8faa985f1b1675428b5e7747d3aa642b
justinveras2546 <justinveras20@gmail.com>
2026-09-04 10:30:36 +0200

    You can switch MoGe versions and change refine

--- src/science_jubilee/Vision/Monocular_Horizontal_leaves/ingredients/inference_MoGe.py
+++ src/science_jubilee/Vision/Monocular_Horizontal_leaves/ingredients/inference_MoGe.py
@@ -12,16 +12,22 @@ def config():
     image_path = "/images.latest.png"
     output_path = "/output"
     resolution_level = 9
+    refinement_steps = 5
+    model_version = 3
 
-def _load_MoGe_modules():
+def _load_MoGe_modules(model_version: int = 3):
     try:
         import torch
-        from moge.model.v2 import MoGeModel 
-
-        device = torch.device("cuda")
-
+        
+        if model_version == 3:
+            from moge.model.v3 import MoGeModel 
+            device = torch.device("cuda")
+            model = MoGeModel.from_pretrained("Ruicheng/moge-3-vitl").to(device)
         # Load the model
-        model = MoGeModel.from_pretrained("Ruicheng/moge-2-vitl-normal").to(device)
+        if model_version == 2:
+            from moge.model.v2 import MoGeModel
+            device = torch.device("cuda")
+            model = MoGeModel.from_pretrained("Ruicheng/moge-2-vitl-normal").to(device)
         model.eval() 
         
     except Exception as exc:  # pragma: no cover - environment dependent
@@ -31,12 +37,12 @@ def _load_MoGe_modules():
 
     return torch, model, device
 
-def infer_depth_and_normals(image_path: str, output_dir: str, resolution_level: int = 9):
+def infer_depth_and_normals(image_path: str, output_dir: str, resolution_level: int = 9,refinement_steps: int = 5, model_version: int = 3):
     image_path = str(image_path)
     output_dir = str(output_dir)
     os.makedirs(output_dir, exist_ok=True)
 
-    torch, model, device = _load_MoGe_modules()
+    torch, model, device = _load_MoGe_modules(model_version=model_version)
     
     image_bgr = cv2.imread(image_path)
     if image_bgr is None:
@@ -47,7 +53,10 @@ def infer_depth_and_normals(image_path: str, output_dir: str, resolution_level: 
     image_tensor = torch.tensor(image_rgb / 255.0, dtype=torch.float32, device=device).permute(2, 0, 1)   
     
     with torch.no_grad():
-        output = model.infer(image_tensor,resolution_level=resolution_level)
+        if model_version == 3:
+            output = model.infer(image_tensor, resolution_level=resolution_level,refine_steps=refinement_steps)
+        elif model_version == 2:
+            output = model.infer(image_tensor, resolution_level=resolution_level)
         
     return output
 
@@ -59,7 +68,7 @@ def to_numpy(tensor):
 
 @inference_MoGe.capture
 def run_infer_depths_and_normals(
-    image, output_dir: str, image_name: str = "image.jpg", resolution_level: int = 9
+    image, output_dir: str, image_name: str = "image.jpg", resolution_level: int = 9, refinement_steps: int = 5, model_version: int = 3
 ):
     output_path = Path(output_dir)
     output_path.mkdir(parents=True, exist_ok=True)
@@ -68,7 +77,7 @@ def run_infer_depths_and_normals(
     cv2.imwrite(str(image_path), np.asarray(image))
     
     predict = infer_depth_and_normals(
-        str(image_path), str(output_path), resolution_level=resolution_level
+        str(image_path), str(output_path), resolution_level=resolution_level,refinement_steps=refinement_steps, model_version=model_version
     )
 
     return {
